25aa03885802755969e8aa111435c96d97141796
User <User@192.168.30.186>
2015-09-04 16:20:26 +0800

    Updated SQL

--- payroll/src/main/resources/payroll.sql
+++ payroll/src/main/resources/payroll.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2015 at 08:44 AM
+-- Generation Time: Sep 04, 2015 at 10:19 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -32,7 +32,15 @@ CREATE TABLE IF NOT EXISTS `cutoff` (
   `enddate` date NOT NULL,
   `workingdays` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `cutoff`
+--
+
+INSERT INTO `cutoff` (`id`, `startdate`, `enddate`, `workingdays`) VALUES
+(11, '2015-01-09', '2016-03-09', 11),
+(12, '2015-01-09', '2016-03-09', 11);
 
 -- --------------------------------------------------------
 
@@ -63,8 +71,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `middlename`, `age`, `sex`, `dateofbirth`, `maritalstatus`, `numberofdependents`, `basicsalary`, `othertaxable`, `nontaxable`, `sickleave`, `vacationleave`) VALUES
-(2, 'Valen', 'Barcinal', '', 22, 'male', '2015-09-01', 'single', 1, 20000, 0, 5000, 10, 10),
-(3, 'Nick', 'Tunac', '', 18, 'Male', '2015-01-09', 'Single', 0, 1111, 2222, 444, 10, 10);
+(2, 'Valen', 'Barcinal', '', 22, 'male', '2015-09-01', 'single', 1, 20000, 0, 5000, 8, 10),
+(3, 'Nick', 'Tunac', '', 18, 'Male', '2015-01-09', 'Single', 0, 1111, 2222, 444, 4, 10);
 
 -- --------------------------------------------------------
 
@@ -90,7 +98,16 @@ CREATE TABLE IF NOT EXISTS `usercutoff` (
   `sickleave` int(11) NOT NULL,
   `vacationleave` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+--
+-- Dumping data for table `usercutoff`
+--
+
+INSERT INTO `usercutoff` (`id`, `cutoffid`, `userid`, `absence`, `overtime`, `tardiness`, `sss`, `pagibig`, `philhealth`, `basicsalary`, `othertaxable`, `nontaxable`, `numberofdependents`, `totalsalary`, `sickleave`, `vacationleave`) VALUES
+(19, 11, 2, 1, 8, 7, 290.65, 200, 218.75, 10000, 0, 2500, 4, 11079.77181818182, 2, 0),
+(21, 12, 2, 0, 0, 0, 290.65, 200, 218.75, 10000, 0, 2500, 1, 10769.95, 0, 0),
+(22, 12, 3, 0, 0, 0, 18.15, 11.11, 50, 555.5, 1111, 222, 0, 1809.24, 2, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
