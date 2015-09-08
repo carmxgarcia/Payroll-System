09179266d2731cc4da50a868d62e4291a8c3ad29
User <User@192.168.30.186>
2015-09-08 16:28:03 +0800

    SQL updated

--- payroll/src/main/resources/payroll.sql
+++ payroll/src/main/resources/payroll.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 04, 2015 at 10:19 AM
+-- Generation Time: Sep 08, 2015 at 10:27 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -41,6 +41,41 @@ CREATE TABLE IF NOT EXISTS `cutoff` (
 INSERT INTO `cutoff` (`id`, `startdate`, `enddate`, `workingdays`) VALUES
 (11, '2015-01-09', '2016-03-09', 11),
 (12, '2015-01-09', '2016-03-09', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_authorities`
+--
+
+CREATE TABLE IF NOT EXISTS `group_authorities` (
+  `authority` varchar(255) NOT NULL,
+  `group_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_members`
+--
+
+CREATE TABLE IF NOT EXISTS `group_members` (
+  `username` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `group_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -108,6 +143,20 @@ INSERT INTO `usercutoff` (`id`, `cutoffid`, `userid`, `absence`, `overtime`, `ta
 (19, 11, 2, 1, 8, 7, 290.65, 200, 218.75, 10000, 0, 2500, 4, 11079.77181818182, 2, 0),
 (21, 12, 2, 0, 0, 0, 290.65, 200, 218.75, 10000, 0, 2500, 1, 10769.95, 0, 0),
 (22, 12, 3, 0, 0, 0, 18.15, 11.11, 50, 555.5, 1111, 222, 0, 1809.24, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
